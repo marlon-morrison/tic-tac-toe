29806d4d353d532cb6df18f12433989f47d21526
marlon-morrison <52177393+marlon-morrison@users.noreply.github.com>
2019-12-17 10:34:30 -0600

    win  conditions

--- js/app.js
+++ js/app.js
@@ -20,20 +20,20 @@ function game(block) {
     block.innerText = turn;
     console.log(letters[0].textContent);
     if (letters[0].textContent && letters[3].textContent && letters[6].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[1].textContent && letters[4].textContent && letters[7].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[2].textContent && letters[5].textContent && letters[8].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[0].textContent && letters[1].textContent && letters[2].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[3].textContent && letters[4].textContent && letters[5].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[6].textContent && letters[7].textContent && letters[8].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[0].textContent && letters[4].textContent && letters[8].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     } else if (letters[2].textContent && letters[4].textContent && letters[6].textContent === "X") {
-        alert("sup")
+        alert("X Wins")
     }
 };
