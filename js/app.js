003c23bd41c82d6505d4a81ead89820df7f45667
marlon-morrison <52177393+marlon-morrison@users.noreply.github.com>
2019-12-17 10:24:44 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+var letters = document.getElementsByClassName("letter");
+for (let i = 0; i < letters.length; i++) {
+    letters[i].addEventListener("click",function () {
+        game(this);
+    });
+};
+var p1 = true;
+
+function game(block) {
+    if (block.innerText) {
+        return;
+    }
+    var turn;
+    this.player = !this.player
+    if (player) {
+        turn = "X"
+    } else {
+        turn = "O"
+    }
+    block.innerText = turn;
+    console.log(letters[0].textContent);
+    if (letters[0].textContent && letters[3].textContent && letters[6].textContent === "X") {
+        alert("sup")
+    } else if (letters[1].textContent && letters[4].textContent && letters[7].textContent === "X") {
+        alert("sup")
+    } else if (letters[2].textContent && letters[5].textContent && letters[8].textContent === "X") {
+        alert("sup")
+    } else if (letters[0].textContent && letters[1].textContent && letters[2].textContent === "X") {
+        alert("sup")
+    } else if (letters[3].textContent && letters[4].textContent && letters[5].textContent === "X") {
+        alert("sup")
+    } else if (letters[6].textContent && letters[7].textContent && letters[8].textContent === "X") {
+        alert("sup")
+    } else if (letters[0].textContent && letters[4].textContent && letters[8].textContent === "X") {
+        alert("sup")
+    } else if (letters[2].textContent && letters[4].textContent && letters[6].textContent === "X") {
+        alert("sup")
+    }
+};
